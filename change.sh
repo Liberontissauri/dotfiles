94a43dba388ca2cac87edba0ad541ea91db11869
Liberontissauri <jp.liberon@gmail.com>
2024-11-17 18:19:11 +0000

    vaporwave theme

--- change.sh
+++ change.sh
@@ -3,8 +3,10 @@
 # Themes:
 # - Sakura Blossom (sakura-blossom)
 # - Sakura Blossom Laptop (sakura-blossom-laptop)
-theme="river-village"
+theme="vaporwave-1"
 
+rm ~/.config/background.png
+ln -s ~/Dotfiles/${theme}/background.png ~/.config/background.png
 for folder in $(ls ./${theme}) ; do
 	echo ./${theme}/${folder}
 	echo  ~/.config/${folder}
